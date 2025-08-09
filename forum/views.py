00f24f318ac08f7c245c7eddecc1d8044dd7f27f
ProgramCodeMasterA <vb7zl582@students.codeinstitute.net>
2025-08-09 08:11:07 +0100

    Add base.html template, Add index.html template

--- forum/views.py
+++ forum/views.py
@@ -6,4 +6,5 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(author=1)
-    template_name = "post_list.html"
+    template_name = "forum/index.html"
+    paginate_by = 6
