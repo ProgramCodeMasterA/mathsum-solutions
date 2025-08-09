fe75ed932a7c1cebe9c136ebbbb3ee2fddef81b4
ProgramCodeMasterA <vb7zl582@students.codeinstitute.net>
2025-08-09 06:24:54 +0100

    Add Admin panel customization

--- forum/admin.py
+++ forum/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('problem', 'context', 'solved', 'created_on')
+    search_fields = ['problem']
+    list_filter = ('solved',)
+    prepopulated_fields = {'context': ('problem',)}
+    summernote_fields = ('context',)
+
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
