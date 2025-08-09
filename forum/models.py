afad88df5f216d37311d98f9657f9428cf6f58a0
ProgramCodeMasterA <vb7zl582@students.codeinstitute.net>
2025-08-09 05:00:59 +0100

    Add POST Comment model

--- forum/models.py
+++ forum/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published"))
+
 DIFFICULTY_CHOICES = (('low','Low'),
     ('moderate', 'Moderate'),
     ('high', 'High'),
@@ -20,11 +20,20 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="forum_posts"
     )
-    content = models.CharField(max_length=100, unique=True) # Type in the mathematical content of your math problem
+    problem = models.CharField(max_length=100, unique=True) # Type in the mathematical content of your math problem
     context = models.TextField(max_length=200) # Type in what answer you expect to get when this mathematical problem is solved
     area = models.CharField(max_length=15, choices=MATH_CHOICES, default='arithmetic')  # Choose which category of Maths the problem is
     difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES, default='low')  # Choose perceived difficulty of problem
-    resolution = models.BooleanField(default=False)    # True/False if math problem is solved
+    solved = models.BooleanField(default=False)    # True/False if math problem is solved or not
     created_on = models.DateTimeField(auto_now_add=True)    # Add DateTime field for when post is created
     updated_on = models.DateTimeField(auto_now_add=True)    # Add DateTime field for when post is updated
-    status = models.IntegerField(choices=STATUS, default=0) # Define if object is a draft or is published post
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    response = models.TextField(max_length=200)     # Response to user's post
+    answer = models.BooleanField(default=False)     # True/False if reply gives correct solution for math problem
+    created_on = models.DateTimeField(auto_now_add=True)    # Add DateTime field for when comment is created
