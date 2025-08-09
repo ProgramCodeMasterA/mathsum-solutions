900f85b2e57ca127cc1e487ec893a48aec00a97e
ProgramCodeMasterA <vb7zl582@students.codeinstitute.net>
2025-08-09 08:44:07 +0100

    added post_detail view

--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.views import generic
 from .models import Post
@@ -8,3 +8,26 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(author=1)
     template_name = "forum/index.html"
     paginate_by = 6
+
+def post_detail(request, context):
+    """
+    Display an individual :model:`forum.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`forum.Post`.
+
+    **Template:**
+
+    :template:`forum/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, context=context)
+
+    return render(
+        request,
+        "forum/post_detail.html",
+        {"post": post},
+    )
